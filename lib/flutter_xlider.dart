9eb46601acc6519072a79b91fdef58834ce3d098
Efra Espada <efraespada@gmail.com>
2022-04-04 15:46:22 +0200

    feature: fixed unmounted exception
    Prevents:
    
    ======== Exception caught by scheduler library =====================================================
    
    This widget has been unmounted, so the State no longer has a context (and should be considered defunct).

--- lib/flutter_xlider.dart
+++ lib/flutter_xlider.dart
@@ -440,6 +440,8 @@ class _FlutterSliderState extends State<FlutterSlider>
                 curve: Curves.fastOutSlowIn));
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+
       _renderBoxInitialization();
 
       _arrangeHandlersPosition();
@@ -2731,4 +2733,5 @@ class FlutterSliderRangeStep {
 }
 
 enum FlutterSliderTooltipDirection { top, left, right }
+
 enum FlutterSliderHatchMarkAlignment { left, right }
